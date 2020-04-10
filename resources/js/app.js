265777e79a1594e5105e9dc6322751e8c9309354
Farhan Asif <f.asif2010@gmail.com>
2020-04-10 17:34:44 +0600

    component and vue setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,21 @@
 require('./bootstrap');
 require('admin-lte');
 
+
+
 window.Vue = require('vue');
+
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+
+
+
+const routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default }
+  ]
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +34,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,6 +42,15 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
+const router = new VueRouter({
+    routes // short for `routes: routes`
 });
+
+// const app = new Vue({
+//     el: '#app',
+//     router
+// });
+
+const app = new Vue({
+    router
+  }).$mount('#app')
